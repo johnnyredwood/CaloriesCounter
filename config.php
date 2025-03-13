3d1b354cadb0ed08f9e341eb2f399a096a6bcce4
John Ochoa <johnkleber.ochoa.contractor@bbva.com>
2025-03-13 13:30:16 -0500

    Update config

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('America/New_York');
-$conexion = mysqli_connect("localhost:3307", "usuario", "Inflamesforu3", "nutrition");
+$conexion = mysqli_connect("localhost:3307", "usuario", "", "nutrition");
 if (mysqli_connect_errno())
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
